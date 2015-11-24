682608145b8981d2ab2b37c61463820255239c8b
SARA GUDEMAN <sgudeman982@gmail.com>
2015-11-23 19:14:43 -0800

    (feat)adds different frequency selection in template + controller

--- cron.controller.js
+++ cron.controller.js
@@ -5,5 +5,47 @@
     .module('cron')
     .controller('CronController', CronController);
 
-  function CronController() {}
+  CronController.$inject = ['$scope', 'utilsFactory'];
+
+  function CronController($scope, utilsFactory) {
+
+    var options = {
+      seconds: utilsFactory.range(0, 60),
+      minutes: utilsFactory.range(0, 60),
+      daysOfWeek: utilsFactory.range(0, 7),
+      daysOfMonth: utilsFactory.range(1, 32),
+      months: utilsFactory.range(0, 12)
+    };
+
+    $scope.frequencyInput = {};
+
+    // modeled after: https://docs.angularjs.org/api/ng/directive/ngOptions
+    $scope.frequencyOptions = [
+      {
+        id : 1,
+        label : 'Second'
+      },
+      {
+        id : 2,
+        label : 'Minute'  
+      },
+      {
+        id : 3,
+        label : 'Hour'
+      },
+      {
+        id : 4,
+        label : 'Day of Month'  
+      },
+      {
+        id : 5,
+        label : 'Month'  
+      },
+      {
+        id : 6,
+        label : 'Day of Week'  
+      }
+    ];
+
+  }
 })();
--- cron.templates.js
+++ cron.templates.js
@@ -4,7 +4,7 @@
   angular
     .module('cron')
     .run(['$templateCache', function($templateCache) {
-      $templateCache.put("cron-select.html", "");
+      $templateCache.put("cron-select.html", "<select ng-model='frequencyInput' ng-options='frequency as frequency.label for frequency in frequencyOptions track by frequency.id'></select>");
     }])
 
 
