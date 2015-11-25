6de8be942b918f8859aa9b7ab142569293db7107
SARA GUDEMAN <sgudeman982@gmail.com>
2015-11-24 17:10:28 -0800

    (feat)adds hardcoded templates for creating view rough draft

--- templates/cron.templates.js
+++ templates/cron.templates.js
@@ -4,9 +4,9 @@
   angular
     .module('cron')
     .run(['$templateCache', function($templateCache) {
-      $templateCache.put("cron-select.html", "<label>Repeat by: <select ng-model='frequencyInput' ng-options='frequency as frequency.label for frequency in frequencyOptions track by frequency.id'></select></label> <hr />{{frequencyInput.label}} {{frequencyInput.values}}");
+      $templateCache.put("cron-select.html", "<div></div>");
       $templateCache.put("tabs-select.html", "<div></div>")
-      $templateCache.put("seconds-select.html", "<label><select ng-model='secondsInput' ng-options='second for second in seconds'>");
+      $templateCache.put("seconds-select.html", "<div></div>");
       $templateCache.put("hours-select.html", "<div></div>");
       $templateCache.put("week-select.html", "<div></div>");
       $templateCache.put("month-select.html", "<div></div>");
