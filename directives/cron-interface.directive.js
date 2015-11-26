5e9f49592e0a07f45829df266c7f08b2865f61fe
SARA GUDEMAN <sgudeman982@gmail.com>
2015-11-26 09:31:52 -0800

    (refactor)moves cron ctrl logic into directive

--- directives/cron-interface.directive.js
+++ directives/cron-interface.directive.js
@@ -5,13 +5,28 @@
     .module('cron')
     .directive('cronInterface', cronInterface);
 
-  function cronInterface() {
+  cronInterface.$inject = ['cronConfig'];
+  function cronInterface (cronConfig) {
     return {
       templateUrl: 'templates/cron.template.html',
-      controller: 'CronController',
+      // controller: 'CronController',
       restrict: 'E',
       scope: {},
-      link: function(scope, element, attrs, controller, transcludeFn) {}
+      link: function(scope, element, attrs) {
+        scope.frequencyOptions = cronConfig.frequencyOptions;
+        scope.frequencyRanges = cronConfig.frequencyRanges;
+
+        scope.state = {
+          selected: 0
+        };
+
+        scope.displayOptions = function(frequency) {
+          return frequency.id <= scope.state.selected && frequency.isOption;
+        };
+
+        scope.frequencySelect;
+        scope.frequencyCreated = {};
+      }
     }
   }
 })();
