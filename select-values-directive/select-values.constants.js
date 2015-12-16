c47991c636669c3e2980a3cb672dbaa459213224
SARA GUDEMAN <sgudeman982@gmail.com>
2015-12-16 14:10:16 -0800

    (feat WIP)creates select values module + adds config constants

---
+++ select-values-directive/select-values.constants.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  angular.module('cron.ui.selectvalues')
+    .constant('selectValueConfig', {
+      hourStep: 1,
+      minuteStep: 1,
+      secondStep: 1
+  });
+  
+})();
+
---
+++ select-values-directive/select-values.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('cron.ui.selectvalues', []);
+
+})();
